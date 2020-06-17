eba31d05211baf4274e1ca8863c91099c434e388
Jinwoong <jinwoongma@gmail.com>
2020-06-17 18:16:39 +0900

    commit

--- front_end/movie_vue/src/components/UserList.vue
+++ front_end/movie_vue/src/components/UserList.vue
@@ -1,10 +1,14 @@
 <template>
   <v-container fluid>
-    <h1 class="pt-3 d-flex justify-content-center ">사용자 권한 수정</h1>
-		<v-flex v-for="user in this.users" :key="user.id" xs6>
-       <v-checkbox light v-model="user.is_staff" :label="`${user.username}: ${user.is_staff.toString()}`" ></v-checkbox>
-		</v-flex>
-		<v-btn color="secondary" class="small" @click="save()">	변경사항 저장</v-btn>
+    <h1 class="pt-3 d-flex justify-content-center">사용자 권한 수정</h1>
+    <v-flex v-for="user in this.users" :key="user.id" xs6>
+      <v-checkbox
+        light
+        v-model="user.is_staff"
+        :label="`${user.username}: ${user.is_staff.toString()}`"
+      ></v-checkbox>
+    </v-flex>
+    <v-btn color="secondary" class="small" @click="save()">변경사항 저장</v-btn>
   </v-container>
 </template>
 
@@ -15,38 +19,54 @@ export default {
   name: "UserList",
   data() {
     return {
-			selected: [],
-			users: [],
+      selected: [],
+      users: []
     };
-	},
-	methods: {
-		getUserList() {
-			const SERVER_URL = 'http://localhost:8000'
-			const token = sessionStorage.getItem("jwt");
-			const options = {
-          headers: {
-            Authorization: "JWT " + token
-          }
-				};
-			axios
-          .get(`${SERVER_URL}/api/v1/userList/`, options)
-          .then(res => {
-						console.log(res);
-						this.users = res.data
-						// this.users.forEach(user => {
-						// 	if (user.is_staff) {
-						// 		this.selected.push(user)
-						// 	}
-						// })
-          })
-          .catch(err => {
-            console.error(err);
-          });
-		}
-	},
-	created() {
-		this.getUserList()
-	}
+  },
+  methods: {
+    getUserList() {
+      const SERVER_URL = "http://localhost:8000";
+      const token = sessionStorage.getItem("jwt");
+      const options = {
+        headers: {
+          Authorization: "JWT " + token
+        }
+      };
+      axios
+        .get(`${SERVER_URL}/api/v1/userList/`, options)
+        .then(res => {
+          console.log(res);
+          this.users = res.data;
+        })
+        .catch(err => {
+          console.error(err);
+        });
+    },
+
+    save() {
+      const SERVER_URL = "http://localhost:8000";
+      const token = sessionStorage.getItem("jwt");
+      const options = {
+        headers: {
+          Authorization: "JWT " + token
+        }
+			};
+			this.users.forEach(user => {
+				const data = {
+					id: user.id,
+					username: user.username,
+					is_staff: user.is_staff
+				}
+				axios.post(`${SERVER_URL}/api/v1/${user.id}/userupdate/`, data, options)
+					.then(res => {
+						console.log(res.data)
+					})
+			})
+    }
+  },
+  created() {
+    this.getUserList();
+  }
 };
 </script>
 
