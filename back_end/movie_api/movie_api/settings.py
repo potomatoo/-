3b7ac87ef49025fee70d8f876f118d23ff12571c
Jinwoong <jinwoongma@gmail.com>
2020-06-18 17:11:23 +0900

    commit

--- back_end/movie_api/movie_api/settings.py
+++ back_end/movie_api/movie_api/settings.py
@@ -12,20 +12,21 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 import datetime
-from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG')
+SECRET_KEY = 'fa8ji)#9^v$ghx&(n)x*rh#$6(l2&5tph&z7nq$b!r5jojw)8j'
 
-ALLOWED_HOSTS = []
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -154,9 +155,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 CORS_ORIGIN_ALLOW_ALL = True
 
 AUTH_USER_MODEL = 'movies.User'
-
